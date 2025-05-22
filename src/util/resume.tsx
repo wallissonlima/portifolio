3d7fa77b6662e086d3a8cc6ae3842aaad9d015d5
Wallisson Lima <82485145+wallissonlima@users.noreply.github.com>
2025-05-22 15:34:40 -0300

    Ajuste na parte do resumo

--- src/util/resume.tsx
+++ src/util/resume.tsx
@@ -1,36 +1,36 @@
 export const ResumeData = {
   experience: [
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Desenvolvedor web",
+      subTitle: "Luziânia, GO",
+      Description: "Desenvolvi um sistema web em ReactJS focado no mapeamento de áreas agrícolas e na análise da fertilidade do solo, com o objetivo de otimizar plantios e colheitas por meio da tecnologia. A solução visa aumentar a precisão no monitoramento das lavouras e auxiliar produtores rurais na tomada de decisões estratégicas. O sistema está integrado a um aplicativo móvel, Visita Técnica, utilizado por técnicos agrícolas durante visitas a campo. Por meio do app, são coletadas informações sobre o estado das plantas, fertilidade do solo, presença de pragas e condições climáticas. Esses dados são transformados em relatórios detalhados, que são compartilhados com os clientes, promovendo agilidade, produtividade e melhores resultados no campo.",
     },
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Desenvolvedor FullStack",
+      subTitle: "Brasilia, DF",
+      Description: "Atuei no aprimoramento da usabilidade do sistema SUPRA, com foco na importação eficiente de dados e na melhoria da experiência do usuário. Implementei funcionalidades para validação de planilhas, garantindo conformidade com os parâmetros exigidos. Otimizei o processo de importação de arquivos de grande porte, resultando em maior agilidade e desempenho no sistema. Além disso, contribuí para o desenvolvimento de interfaces de usuário intuitivas, como telas de login e módulos funcionais, visando facilitar a navegação e a interação dos usuários com a plataforma.",
     },
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Desenvolvedor FullStack",
+      subTitle: "Brasilia, DF",
+      Description: "Atuo como desenvolvedor FullStack, com domínio tanto do backend quanto do frontend. Tenho sólida experiência em Node.js, desenvolvendo APIs escaláveis e performáticas com foco em eficiência e arquitetura assíncrona. No frontend, trabalho com React.js, criando interfaces modernas, responsivas e interativas por meio de componentização e gerenciamento de estado. Minha atuação visa entregar aplicações web completas, robustas e com excelente experiência para o usuário.",
     },
   ],
   education: [
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Sistemas de informação",
+      subTitle: "Luziânia, GO",
+      Description: "Bacharelado - Sistemas de informação",
     },
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Rocketseat",
+      subTitle: "São paulo, SP",
+      Description: "Certificação em Desenvolvimento Web Full (Rocketseat) Certificações",
     },
     {
-      title: "Consultant",
-      subTitle: "Miami, Fl",
-      Description: "User experience, visual design",
+      title: "Wizard by Pearson",
+      subTitle: "Luziânia, GO",
+      Description: "Ensino de Inglês como Segundo Idioma",
     },
   ],
 };
